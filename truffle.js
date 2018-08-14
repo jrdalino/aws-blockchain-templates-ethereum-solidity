38acfabc70a9c9825df8b51f0a25d89a1583c5e1
Dalino <jrdalino@c4b301d36fc1.ant.amazon.com>
2018-08-14 11:54:45 +0800

    added awsNetwork in truffle.js

--- truffle.js
+++ truffle.js
@@ -1,11 +1,20 @@
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+var mnemonic = "outdoor father modify...";
+const privateKeys = ["afd21..."]; // private keys
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    awsNetwork: {
+      provider: () => {
+        return new HDWalletProvider(privateKeys, "http://internal-MyFir-LoadB-1ST6DYDCSRRUF-907719992.us-east-1.elb.amazonaws.com")
+      },
+      port: 8545,
+      network_id: 1234
     }
   }
 };
